f196164c1bb9f6e848e3fa74da2dbbdb717d63ce
Matt Radford <quinquex@gmail.com>
2025-03-12 09:37:39 -0700

    list all pets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import { useState, useEffect } from 'react';
+import * as petService from './services/petService.js';
+import PetList from './components/PetList/PetList.jsx';
+import './App.css';
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+
+  const [pets, setPets] = useState([]);
+  const [selected, setSelected] = useState(null);
+
+  useEffect(() => { // useEffect runs on page load
+    const fetchPets = async () => { // can't have async directly inside useEffect, must nest
+      try {
+        const fetchedPets = await petService.index();
+        if (fetchedPets.err) {
+          throw new Error(fetchedPets.err);
+        };
+        setPets(fetchedPets);
+      } catch (error) {
+        console.log(error);
+      };
+    };
+    fetchPets();
+  }, []);
+
+  const handleSelect = (pet) => {
+    setSelected(pet);
+  };
+
+  return (
+    <>
+      <PetList pets={pets} handleSelect={handleSelect}/>
+    </>
+  );
 };
 
 export default App;
---
+++ src/services/petService.js
@@ -0,0 +1,16 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`;
+
+const index = async () => {
+    try {
+        const res = await fetch(BASE_URL); // GET /pets
+        return res.json();
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+console.log(index());
+
+export {
+    index,
+};
